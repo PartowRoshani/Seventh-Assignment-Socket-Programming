aa0d2da1037254a8bcda953eb9b23260608c6308
Partow Roshani <roshanipartow@gmail.com>
2025-05-21 20:12:33 +0330

    Debug Upload

--- src/main/java/Client/Client.java
+++ src/main/java/Client/Client.java
@@ -117,7 +117,7 @@ public class Client {
     private static void uploadFile(Scanner scanner) throws IOException {
 
         //TODO: list all files in the resources/Client/<username> folder
-        File userDirectory = new File("C:\\Users\\User\\Desktop\\AP\\Seventh-Assignment-Socket-Programming\\src\\main\\resources\\Client\\user1" + username);
+        File userDirectory = new File("C:\\Users\\User\\Desktop\\AP\\Seventh-Assignment-Socket-Programming\\src\\main\\resources\\Client\\" + username);
         File[] files = userDirectory.listFiles((dir, name) -> new File(dir, name).isFile());
 
         if (files == null || files.length == 0) {
@@ -196,7 +196,7 @@ public class Client {
         out.println("DOWNLOAD_FILE "+fileNames[choice]);
 
         long fileSize = Long.parseLong(in.readLine());
-        File userDir = new File("resources/Client/" + username);
+        File userDir = new File("C:\\Users\\User\\Desktop\\AP\\Seventh-Assignment-Socket-Programming\\src\\main\\resources\\Client\\" + username);
         if (!userDir.exists())
         {
             userDir.mkdirs();
